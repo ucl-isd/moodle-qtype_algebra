c530c737a78835e8803cd7f898af24a830cf6ae4
Matthias Opitz <opitz@gmx.de>
2025-10-15 11:32:58 +0100

    revoked renamimg class qtype_algebra_test_helper

--- tests/question_type_test.php
+++ tests/question_type_test.php
@@ -28,7 +28,7 @@ use coding_exception;
 use moodle_exception;
 use qtype_algebra;
 use qtype_algebra_edit_form;
-use helper;
+use qtype_algebra_test_helper;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -150,7 +150,7 @@ final class question_type_test extends \advanced_testcase {
         $formdata->category = "{$cat->id},{$cat->contextid}";
         qtype_algebra_edit_form::mock_submit((array)$formdata);
 
-        $form = helper::get_question_editing_form($cat, $questiondata);
+        $form = qtype_algebra_test_helper::get_question_editing_form($cat, $questiondata);
 
         $this->assertTrue($form->is_validated());
 
@@ -205,7 +205,7 @@ final class question_type_test extends \advanced_testcase {
         $formdata->answer[0] = '   7*x   ';
         qtype_algebra_edit_form::mock_submit((array)$formdata);
 
-        $form = helper::get_question_editing_form($cat, $questiondata);
+        $form = qtype_algebra_test_helper::get_question_editing_form($cat, $questiondata);
 
         $this->assertTrue($form->is_validated());
 
